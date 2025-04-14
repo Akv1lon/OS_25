e966678bc668ae5a9e9a3e8754b4cadb561ce256
Yurchenya <wizardman14@mail.ru>
2025-04-14 17:03:51 +0700

    Программы с разным поведением ребенка и родителя

---
+++ pr005.c
@@ -0,0 +1,39 @@
+//Программа, порождающая дочерний процесс (часть 2).
+//Процесс-родитель и процесс-ребёнок должны выполнят разные действия
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	pid_t procid, pprocid;
+        int a=0, b;
+        procid = getpid();
+        pprocid = getppid();
+        printf("До вызова fork()\n");
+        printf("ID текущего процесса %d\nID родительского процесса: %d\n", procid, pprocid);
+        printf("Начальное значение a: %d\n", a);
+        b = fork();
+	if (b<0) {
+		//Во вреия выполнения fork() произошла ошибка. Выходим из программы с возвратом кода -1
+		printf("Ошибка при выполнении fork()\n");
+		exit(-1); //выход из программы
+	}
+	else if(b==0){
+        	printf("\n\nПоявился дочерний процесс, получил значение %d\n", b);
+		a = a+1; //проверяем изменениие значений переиенной a
+        	procid = getpid();
+        	pprocid = getppid();
+        	printf("После  вызова fork() в процессе-ребёнке\n");
+        	printf("ID текущего процесса %d\nID родительского процесса: %d\nНовое значение a: %d\n", procid, pprocid, a);
+	}
+	else {
+		printf("\n\nРодительский процесс после вызова fork() получил ID ребёнка: %d\n",b);
+		a = a+1111; //проверяем изменениие значений переиенной a
+		procid = getpid();
+		pprocid = getppid();
+		printf("Идентификаторы процесса-родителя:\n");	
+		printf("ID текущего процесса %d\nID родительского процесса: %d\nНовое значение a: %d\n", procid, pprocid, a);
+	}
+	return 0;
+}
